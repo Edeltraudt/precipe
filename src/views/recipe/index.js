a3331aeee805b984cfbc8b38567c73009ac28f37
Patricia Steves <pats@hotmail.de>
2020-03-29 19:03:10 +0200

    Add basic recipe view

--- src/views/recipe/index.js
+++ src/views/recipe/index.js
@@ -1,11 +1,133 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import { RecipeIngredient } from "./../../components/recipes";
+import { UserIconGroup } from "./../../components/user/icons";
+import { Headline } from "./../../components/core/typography";
+
+import placeholder from "./../../assets/img/recipe-detail-placeholder.jpg";
 
 import styles from "./recipe.module.scss";
 
+const recipes = [
+  {
+    id: 1,
+    title: "Simple beet carpaccio with mixed herbs",
+    duration: 20,
+    servings: 4,
+    ingredients: [
+      {
+        name: "red beets",
+        amount: 4,
+        unit: null
+      },
+      {
+        name: "red onion",
+        amount: 1,
+        unit: null
+      },
+      {
+        name: "mixed herbs",
+        amount: 20,
+        unit: "g"
+      },
+      {
+        name: "olive oil",
+        amount: 50,
+        unit: "g"
+      },
+      {
+        name: "lemon",
+        amount: 1,
+        unit: null
+      },
+      {
+        name: "sugar",
+        amount: 1,
+        unit: "tsb"
+      },
+      {
+        name: "flaky sea salt",
+        amount: null,
+        unit: null
+      }
+    ],
+    instructions:
+      "<p>Peel, halve, and thinly slice red onion. Remove leaves from herb stems and mince. Zest and juice lemon and add to a bowl with olive oil, sugar, capers, and salt. Season with pepper and mix well.</p><p>Peel and thinly slice beetroots with a mandoline. Add to bowl with the dressing and mix.</p><p>Layer beets on a plate and drizzle with olive oil. Garnish with flaky sea salt and more mixed herbs. Enjoy!</p>"
+  },
+  {
+    id: 2,
+    title: "Simple beet carpaccio with mixed herbs 2",
+    duration: 20,
+    servings: 4,
+    ingredients: [
+      {
+        name: "red beets",
+        amount: 4,
+        unit: null
+      }
+    ],
+    instructions:
+      "<p>Peel, halve, and thinly slice red onion. Remove leaves from herb stems and mince. Zest and juice lemon and add to a bowl with olive oil, sugar, capers, and salt. Season with pepper and mix well.</p><p>Peel and thinly slice beetroots with a mandoline. Add to bowl with the dressing and mix.</p><p>Layer beets on a plate and drizzle with olive oil. Garnish with flaky sea salt and more mixed herbs. Enjoy!</p>"
+  }
+];
+
 const RecipeView = props => {
+  const { id } = useParams();
+
+  let recipe;
+
+  if (!Number.isNaN(id)) {
+    recipe = recipes.find(recipe => recipe.id === Number(id));
+    recipe.image = placeholder;
+  }
+
   return (
-    <main className={`${styles.Dashboard} wrap`}>
-    </main>
+    <div className={styles.Recipe}>
+      {recipe && (
+        <>
+          <header className={styles.Header}>
+            <Headline level="2" type="title">
+              {recipe.title}
+            </Headline>
+            <div className={styles.Info}>
+              <UserIconGroup editable groupId={1} />
+            </div>
+          </header>
+          <div className={styles.Main}>
+            <div className={styles.ImageWrap}>
+              <figure className={styles.Image}>
+                <img src={recipe.image} alt=""/>
+              </figure>
+            </div>
+            <section className={`${styles.Ingredients} card`}>
+              <header className={`${styles.IngredientsHeader} card__header`}>
+                <Headline level="3" type="label">
+                  Ingredients
+                </Headline>
+              </header>
+              <ul className={styles.IngredientsList}>
+                {recipe.ingredients.map(ingredient => (
+                  <li className={styles.IngredientsItem}>
+                    <RecipeIngredient
+                      qty={ingredient.amount}
+                      unit={ingredient.unit}
+                      name={ingredient.name}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </div>
+          <section className={styles.Preparation}>
+            <Headline level="3" type="label">
+              Preparation
+            </Headline>
+            <div dangerouslySetInnerHTML={{__html: recipe.instructions}} />
+          </section>
+        </>
+      )}
+    </div>
   );
 };
 
